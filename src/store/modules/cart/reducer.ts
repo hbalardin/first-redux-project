75e1d2cb1f729c0787010b31a9dd050eb99e7807
Henrique Balardin <balardinh@gmail.com>
2021-08-26 14:32:45 -0300

    incrementing product quantity instead add new product

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -12,10 +12,18 @@ export const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
       case ActionTypes.addProductToCart: {
         const { product } = action.payload;
 
-        draft.items.push({
-          product,
-          quantity: 1,
-        });
+        const productInCartIndex = draft.items.findIndex(
+          (item) => item.product.id === product.id
+        );
+
+        if (productInCartIndex >= 0) {
+          draft.items[productInCartIndex].quantity++;
+        } else {
+          draft.items.push({
+            product,
+            quantity: 1,
+          });
+        }
         break;
       }
       default: {
