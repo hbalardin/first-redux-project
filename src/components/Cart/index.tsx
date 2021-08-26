ef2cc74d1d820e8d08af33ba023e050170a825e8
Henrique Balardin <balardinh@gmail.com>
2021-08-26 13:57:37 -0300

    render items on cart component

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { IState } from '../../store';
+import { ICartItem } from '../../store/modules/cart/types';
 
 export const Cart: React.FC = () => {
+  const cart = useSelector<IState, ICartItem[]>((state) => state.cart.items);
+
   return (
     <table>
       <thead>
@@ -11,7 +16,16 @@ export const Cart: React.FC = () => {
           <th>Subtotal</th>
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>
+        {cart.map((item) => (
+          <tr key={item.product.id}>
+            <td>{item.product.title}</td>
+            <td>{item.product.price}</td>
+            <td>{item.quantity}</td>
+            <td>{(item.product.price * item.quantity).toFixed(2)}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,9 @@
 import { createStore } from 'redux';
+import { ICartState } from './modules/cart/types';
 import rootReducer from './modules/rootReducer';
+
+export interface IState {
+  cart: ICartState;
+}
 
 export const store = createStore(rootReducer);
